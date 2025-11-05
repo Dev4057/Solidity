4198cdfea2bc5bdd89d13e42bec2f9818d70e722
Dev4057 <devanggandhi04@gmail.com>
2025-11-05 17:51:36 +0000

    Adding some Comments

--- foundry-simple-storage-f23/src/contracts/SimpleNFT.sol
+++ foundry-simple-storage-f23/src/contracts/SimpleNFT.sol
@@ -10,6 +10,7 @@ contract SimpleNFT{
 
     event Transfer(address indexed  from , address indexed to , uint indexed tokenID);
 
+    // This function actually lets you mint the new token 
     function mint()external returns (uint){
         uint tokenId= nextTokenId++;
         ownerOf[tokenId]=msg.sender;
@@ -17,12 +18,14 @@ contract SimpleNFT{
         emit Transfer(address(0),msg.sender ,tokenId); // Base address ke pass se msg.sender ko send karo and with a particular token Id 
     }
 
+
+
     // Below function lets you mint the existin token to some one else accordingly  
     function transferFrom(address from , address to, uint tokenId)external {
 
-        require(ownerOf[tokenId]==from ,"Not Owner");
-        require(msg.sender==from ,"Not Approved");
-        ownerOf[tokenId]==to;
+        require(ownerOf[tokenId]==from ,"Not Owner");// Yeh  pehle token woh particular sender ka hina chahiye 
+        require(msg.sender==from ,"Not Approved"); // and jo bhej rha  hai token who khud woh token bhejne wala chaiye 
+        ownerOf[tokenId]==to;// After that the token Id is given to that next person and then the balance is managed accordingly 
         balanceOf[from]+=1;
         balanceOf[to]-=1;
         emit Transfer(from, to, tokenId);
