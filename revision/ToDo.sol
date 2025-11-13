caac22071fa0842a7e51c6488719450dedaaf7e8
Dev4057 <devanggandhi04@gmail.com>
2025-11-13 05:23:53 +0000

    Fixed: ToDo.sol

--- revision/ToDo.sol
+++ revision/ToDo.sol
@@ -21,21 +21,21 @@ contract toDoList{
 
 
     function executeTask(uint _taskId)external {
-        require(_taskId<Task[msg.sender].length,"Invalid Task Id ");
+        require(_taskId<tasks[msg.sender].length,"Invalid Task Id ");
         tasks[msg.sender][_taskId].pending=true;
         emit TaskExecuted(msg.sender,_taskId);
     }
 
 
     function getMyAllTask() external view returns (Task[] memory){
-         return Task[msg.sender];// Tu isko kaise bolega Tasks ka array for that particular message.sender jisne bhi  ye diya hai 
+         return tasks[msg.sender];// Tu isko kaise bolega Tasks ka array for that particular message.sender jisne bhi  ye diya hai 
     }
 
 
 
     function deleteATask(uint _taskId)external {
-         require(_taskId<Task[msg.sender].length,"Invalid Task Id ");
-         tasks[msg.sender][_taskId]= tasks[msg.sender][tasks[msg.sender][_taskId].length-1];//// Move the last task into the deleted spot, then pop so that the task Id will get updated and then it will not be confused.
+         require(_taskId<tasks[msg.sender].length,"Invalid Task Id ");
+         tasks[msg.sender][_taskId]= tasks[msg.sender][tasks[msg.sender].length-1];//// Move the last task into the deleted spot, then pop so that the task Id will get updated and then it will not be confused.
          tasks[msg.sender].pop();
          emit TaskDeleted(msg.sender,_taskId);
     }
